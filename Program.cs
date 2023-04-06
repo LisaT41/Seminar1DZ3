6c39df8733c2d4f50baedeb9233a7dfc6ea258c7
l.gor41000@yandex.ru <l.gor41000@yandex.ru>
2023-04-06 21:19:20 +0200

    Third task Seminar1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿//  Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+System.Console.Write("Write your number");
+string value;
+value = Console.ReadLine();
+int number = Convert.ToInt32(value);
+
+if (number % 2 == 0)
+{
+    System.Console.WriteLine("paired");
+}
+else
+{
+    System.Console.WriteLine("unpaired");
+}
